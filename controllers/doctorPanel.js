ec31e01742776c92880b67c60d5cb5d1999904d5
Rafael Pawlos <rafael.pawlos@gmail.com>
2018-04-25 01:24:11 +0200

    Implement  patient switch in doctor panel

--- controllers/doctorPanel.js
+++ controllers/doctorPanel.js
@@ -2,6 +2,7 @@ const pageTpl = require('../templates/page');
 const doctorPanelTpl = require('../templates/doctorPanel');
 const t = require('../utils/translate');
 const User = require('../models/User');
+const db = require('../utils/mongodb');
 
 async function handle(request, response) {
   const user = await User.loggedIn(request);
@@ -12,13 +13,24 @@ async function handle(request, response) {
     return true;
   }
 
+  let patients;
+
+  if (user.patientsID && user.patientsID.length > 0) {
+    patients = await (await db.getCollection('Users')).find({
+      _id: {$in: user.patientsID}
+    }, {
+      projection: {username: 1}
+    }).toArray();
+  }
+
   response.write(pageTpl({
     title: t('doctorPanel'),
     pageClass: 'page--doctor-panel',
     content: doctorPanelTpl({
-      content: 'test content'
+      patients: patients
     }),
-    user: user
+    user: user,
+    scripts: ['/public/js/doctorPanel.js']
   }));
 
   return true;
--- templates/doctorPanel.js
+++ templates/doctorPanel.js
@@ -1,5 +1,23 @@
 const tpl = require('../utils/tpl');
+const t = require('../utils/translate');
 
 module.exports = v => {
-  return v.content;
+  return tpl.process`
+  <div class="panel doctor-panel">
+    <div class="panel__item">
+      <label for="patientSelect">${t('choosePatient')}</label>
+      <div class="simple-select select-patient">
+        <select id="doctorSelect">
+              <option value="none">${t('none')}</option>
+          ${v.patients && v.patients.map(p => `
+              <option value="${p._id.toString()}">${p.username}</option>
+          `).join('')}
+        </select>
+      </div>
+    </div>
+    <div class="panel__item">
+       <div id="chartWrapper" class="chart-wrapper"></div>
+    </div>
+  </div>`;
 };
+
